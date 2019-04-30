2216055f0d6d4a61a0ec11c91e74762e3770265a
梁彪彪 <pokerface0206@163.com>
2019-04-30 17:05:38 +0800

    版本1.1

--- js/common.js
+++ js/common.js
@@ -2,32 +2,41 @@
 
 // app.$forceUpdate(); //重新渲染dom元素 
 
+// 原生等待框的样式
+// plus.nativeUI.showWaiting("等待中...", {
+// 	"width": "100%",
+// 	"height": "100%",
+// 	"size": "18px",
+// 	"size": "1px"
+// });
+
 // 在签约的时候传的参数status    用户端强制为1
 
 // 审核结果    1：待审核    2：同意     3：拒绝
 
 // 本地存储的所有数据: 
-	// userStatus  0: 未登录   1: 已登录但是未绑定身份证号码   2: 用户状态正常
-	// userName    绑定用户的名字
-	// areaCode    地区编码
-	// idCard      身份证号码
-	// genderCd    性别
-	// birthDate   出生日期
-	// address     地址
-	// isContrct   是否可以进行签约
-	// userId      个人Id
-	// teamCode    签约的团队code    没有签约任何团队时显示 "暂未签约"
-	// personId    用户的ID；
-	
-	// ryAppKey    融云APPKEY
-	// ryToken	   融云token
-const JTURL = "http://192.168.1.188:8089/jkda/"
+// userStatus  0: 未登录   1: 已登录但是未绑定身份证号码   2: 用户状态正常
+// userName    绑定用户的名字
+// areaCode    地区编码
+// idCard      身份证号码
+// genderCd    性别
+// birthDate   出生日期
+// address     地址
+// isContrct   是否可以进行签约
+// userId      个人Id
+// teamCode    签约的团队code    没有签约任何团队时显示 "暂未签约"
+// personId    用户的ID；
+
+// ryAppKey    融云APPKEY
+// ryToken	   融云token
+const JTURL = "http://47.93.33.135:8089/jkda/"
 // const JTURL = "http://192.168.1.4:8089/jkda/"
 
 
 // 获取本地存储的数据
-var token,phone,areaCode,idCard,personId,userName,userStatus;
-function getStorageDate(){
+var token, phone, areaCode, idCard, personId, userName, userStatus;
+
+function getStorageDate() {
 	token = plus.storage.getItem("token");
 	phone = plus.storage.getItem("phone");
 	areaCode = plus.storage.getItem("areaCode");
@@ -36,18 +45,18 @@ function getStorageDate(){
 	personId = plus.storage.getItem("personId");
 	userName = plus.storage.getItem("userName");
 	userStatus = plus.storage.getItem("userStatus");
-	
+
 }
 
 // 清楚本地所有缓存
-function clearStorage (){
+function clearStorage() {
 	plus.storage.clear();
 }
 
 
 // 盘点某DOM是否含有某class类
 function hasClass(element, cls) {
-    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;   // true为有   false为没有
+	return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1; // true为有   false为没有
 }
 
 // 关闭当前页面
@@ -58,14 +67,14 @@ function closeActivePage() {
 
 // Ajax请求失败后执行的函数
 function ajaxError(type) {
-	if(type == 'timeout') {
+	if (type == 'timeout') {
 		plus.nativeUI.toast("请求超时");
 	} else {
 		plus.nativeUI.toast("请求失败");
 	}
 }
 // token失效的时候调用的函数
-function ajaxTokenInvalid (){
+function ajaxTokenInvalid() {
 	plus.nativeUI.toast("登录凭证失效 请重新登录");
 	plus.storage.removeItem("token");
 	plus.storage.removeItem("userId");
@@ -75,25 +84,23 @@ function ajaxTokenInvalid (){
 	// plus.webview.show(login, "slide-in-left", 300)
 }
 // 其他错误
-function otherErr (){
+function otherErr() {
 	plus.nativeUI.toast("服务器异常 请稍后重试");
 }
 
 
 
 // 刷新首页四个页面的内容
-function refreshIndexPage(){
+function refreshIndexPage() {
 	var indexCom = plus.webview.getWebviewById(plus.runtime.appid);
 	var index_ = plus.webview.getWebviewById("index_");
 	var msg = plus.webview.getWebviewById("msg");
 	var heathy = plus.webview.getWebviewById("heathy");
 	var own = plus.webview.getWebviewById("own");
-	
+
 	mui.fire(indexCom, "refresh");
 	mui.fire(index_, "refresh");
 	mui.fire(msg, "refresh");
 	mui.fire(heathy, "refresh");
 	mui.fire(own, "refresh");
 }
-
-
